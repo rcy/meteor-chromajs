cb676423f630975b6fb9ecbabb458c81fa1d4b41
Ryan Yeske <rcyeske@gmail.com>
2014-11-13 15:32:33 -0800

    first commit

---
+++ chroma-tests.js
@@ -0,0 +1,3 @@
+Tinytest.add('example', function (test) {
+  test.equal(chroma('#D4F880').darken().hex(), "#9bc04b");
+});
---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'rcy:chromajs',
+  summary: 'library for all kinds of color manipulations',
+  version: '0.6.1',
+  git: "https://github.com/rcy/meteor-chromajs"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+
+  api.addFiles('lib/module-before.js', 'client');
+  api.addFiles('lib/chroma.js', 'client');
+  api.addFiles('lib/module-after.js', 'client');
+
+  api.export('chroma', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('rcy:chromajs');
+  api.addFiles('chroma-tests.js', 'client');
+});
